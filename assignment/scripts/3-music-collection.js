5f1a395186bbd86dbee6a814ef0f1aa5395313f0
AnthonyDampier <Dampieranthony2@gmail.com>
2022-10-20 11:41:50 -0500

    Cleaned up code

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -5,12 +5,12 @@ let collection = [];
 // 'addToCollection' function to take inputs 'title', 'artist', 'yearPublished'
 function addToCollection(title, artist, yearPublished, tracks){
     //create album with 'title', 'artist', 'yearPublished', 'track'[]
-    tracks = tracks;
     album = {title, artist, yearPublished, tracks};
+    //adds album to collection
     collection.push(album);
 }
 
-// Created 6 albums in collection array
+// Created 8 albums in collection array
 addToCollection("title1", "artist1", "2021", [['track1', '2:23'],['track2', '3:33']]);
 addToCollection("title2", "artist2", "2022", [['track55', '2:23'],['track66', '3:33'], ['track77', '4:44']]);
 addToCollection("title3", "artist3", "2021", [['appletrack', '2:11']]);
@@ -27,7 +27,7 @@ function showCollection(){
     console.log('Length of collection:',collection.length);
     for (album of collection){
         console.log(album.title.toUpperCase(),' by ', album.artist.toUpperCase(),', published in ', album.yearPublished);
-        // Prints all tracks if there are tracks.
+        // Prints all tracks; if tracks exist.
         if(album.tracks!= undefined){
             for (track of album.tracks){
                 console.log('Name:', track[0],'Duration:', track[1]);
@@ -77,9 +77,6 @@ function search(artist, year, trackName){
                 //console.log('found Artist in ', collection[i].title);
                 foundThruSearch.push(collection[i]);
             }
-            else {
-                //console.log('Not Found');
-            }
         }
         return foundThruSearch;
     }
@@ -98,28 +95,19 @@ function search(artist, year, trackName){
                         return collection[i];
                     }
                 }
-                if (confirm('Do you want to add searched track?')){
-                    console.log('track add to album');
+                // track was not found in albums; add track to album on confirmation
+                if (confirm('Do you want to add missing track?')){
+                    console.log(trackName, "was added to album on confirmation");
                     // console.log(collection[i]);
                     collection[i].tracks.push(track);
                     // console.log(collection[i]);
                 }
-                if (album == undefined){
-
-                }
-
-                return 'NOT FOUND';
-                // track was not found in albums; add track to album
-                //console.log(collection[i].tracks.length);
+                return [];
             }
-
-            // else {
-            //     return 'Not Found';
-
         }
         //return foundThruSearch;
     }
-    return 'NOT FOUND';
+    return [];
 }
 
 // console.log('Expect to output title1 & title5', search('artist1', '2021'));
@@ -130,8 +118,8 @@ console.log('*** search() ***');
 //track testing
 console.log('Expect to output title1:', search('artist1', '2021','track1'));
 console.log('Expect to output title3:', search("artist3", "2021", 'appletrack'));
-console.log('Expect to out NOT FOUND: ', search("Artist1", "2021", 'track3'));
+console.log('Expect to output [] (NOT FOUND): ', search("Artist1", "2021", 'track3'));
 console.log('Expect track found in title4: ', search("Artist4", "2021", 'track2'));
 
-console.log('*** add track to album');
-console.log('Expect to out title1: ', search("Artist1", "2021", 'track3'));
+console.log('*** Y/N : add track to album ***');
+console.log('Expect to out title1 (OK) or [] (Cancel): ', search("Artist1", "2021", 'track3'));
